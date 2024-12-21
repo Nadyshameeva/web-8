15b5309d5b2d968ee99d5a86ca1409fcae7180f1
Nadyshameeva <shameeva.nadya24@gmail.com>
2024-12-22 03:39:48 +0700

    bibi

--- task2.js
+++ task2.js
@@ -1,1 +1,28 @@
-// const button
+const buttons = document.getElementsByClassName('submit-btn');
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('mouseover', () => {
+        console.log('fsdhfklsdjlf')
+        buttons[i].style.background = 'transparent';
+    });
+
+    buttons[i].addEventListener('mouseout', () => {
+        buttons[i].style.background = '#C2AB99';
+    });
+}
+
+let isMars = true;
+function changeBackground() {
+    const back = document.querySelector('header');
+
+    if (isMars) {
+        back.style.background = "url('bg_space.png')";
+    } else {
+        back.style.background = "url('bg_mars.png')";
+    }
+
+    back.style.backgroundPosition = 'center';
+    back.style.backgroundSize = 'cover';
+
+    isMars = !isMars;
+}
